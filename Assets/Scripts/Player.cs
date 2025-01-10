35490f4c7d7a3c5fdb86d6551423cabdaf5472fe
sdemattia <sdematti@uci.edu>
2025-01-10 11:51:25 -0800

    HW1 commit1 - sdemattia
    Movement
    Started - planting seeds, UI

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,18 +11,49 @@ public class Player : MonoBehaviour
     private int _numSeedsLeft;
     private int _numSeedsPlanted;
 
-    private void Start ()
+    private void Start()
     {
         
     }
 
     private void Update()
     {
-        
+        // Up movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            _playerTransform.Translate(Vector3.up * _speed * Time.deltaTime);
+        }
+
+        // Left movement
+        if (Input.GetKey(KeyCode.A))
+        {
+            _playerTransform.Translate(Vector3.left * _speed * Time.deltaTime);
+        }
+
+        // Down movement
+        if (Input.GetKey(KeyCode.S))
+        {
+            _playerTransform.Translate(Vector3.down * _speed * Time.deltaTime);
+        }
+
+        // Right movement
+        if (Input.GetKey(KeyCode.D))
+        {
+            _playerTransform.Translate(Vector3.right * _speed * Time.deltaTime);    
+        }
+
+        // Plants one seed
+        if (Input.GetKey(KeyCode.Space))
+        {
+            PlantSeed();
+        }
     }
 
-    public void PlantSeed ()
+    public void PlantSeed()
     {
-        
+        if (_numSeedsLeft > 0)
+        {
+            
+        }
     }
 }
